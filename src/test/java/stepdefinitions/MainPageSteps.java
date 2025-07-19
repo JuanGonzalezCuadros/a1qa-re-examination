4874e5cb11519f0f4a17fdccc8c487c867f92537
JuanGonzalezCuadros <juan.gonzalez.cuadros@gmail.com>
2025-07-18 20:57:04 -0600

    Add files via upload

---
+++ src/test/java/stepdefinitions/MainPageSteps.java
@@ -0,0 +1,25 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static io.qameta.allure.Allure.step;
+
+import org.testng.Assert;
+
+import pages.MainPage;
+
+public class MainPageSteps {
+    MainPage mainPage = new MainPage();
+
+    @When("I select the {string} link and click it")
+    public void goTo(String page) {
+        step("I select the " + page + " link and click it");
+        mainPage.clickNavigationLink(page);
+    }
+
+    @Then("the main page is displayed")
+    public void isMainPageIsDisplayed() {
+        step("the main page is displayed");
+        Assert.assertTrue(mainPage.state().isDisplayed(), "The main page is not displayed");
+    }
+}
---
+++ src/test/java/stepdefinitions/WindowHandlersPageSteps.java
@@ -0,0 +1,25 @@
+package stepdefinitions;
+
+import org.testng.Assert;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static io.qameta.allure.Allure.step;
+import pages.WindowHandlersPage;
+
+public class WindowHandlersPageSteps {
+    WindowHandlersPage windowHandlersPage = new WindowHandlersPage();
+
+    @Then("the Click Here link is displayed")
+    public void isClickHereLinkDisplayed() {
+        step("the Click Here link is displayed");
+        Assert.assertTrue(windowHandlersPage.isClickHereLinkDisplayed(), "The Click Here link is not displayed");
+    }
+
+    @When("I click the Click here link")
+    public void clickNewPageLink() {
+        step("I click the Click here link");
+        windowHandlersPage.clickNewPageLink();
+    }
+}
